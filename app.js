e13b8b19297a15ddd676e7f21be801f0b5969562
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-06-23 06:34:22 -0400

    added copied message

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const itemSelect = document.querySelector('#items')
 const keySelect = document.querySelector('#keys')
 const generateBtn = document.querySelector('#generateBtn')
 const copyBtn = document.querySelector('#copyBtn')
+const message = document.querySelector('#message')
 
 generateBtn.addEventListener('click', () => {
+    setMessage()
     const item = itemSelect.value
     const key = keySelect.value
     keybindSpan.innerText = "bind " + key +  " buy " + item;
@@ -14,9 +16,15 @@ copyBtn.addEventListener('click',() => {
     try {
         navigator.clipboard.writeText(keybindSpan.innerText)
         if (!keybindSpan.innerText) {
+            setMessage('Nothing to copy!', 'has-text-danger')
             throw new Error("Nothing to copy")
-        }
+        } setMessage('Copied!', 'has-text-info')
     } catch (error) {
         console.error(error)
     }
 })
+
+function setMessage(content = null, color = null) {
+    message.innerText = content;
+    message.className = color
+}
